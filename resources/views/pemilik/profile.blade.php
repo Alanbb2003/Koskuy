e2a6aa3f5d77b241357951f78a46103f64f1ab85
jonathankl <jonathan_k20@mhs.istts.ac.id>
2022-11-29 11:07:43 +0700

    Closed #11

--- resources/views/pemilik/profile.blade.php
+++ resources/views/pemilik/profile.blade.php
@@ -6,15 +6,15 @@
 
 
 <h1>Edit Profile</h1>
-<form action="" method="post">
-
+<form action="{{url('/owner/editprofile')}}" method="post">
+    @csrf
     Nama Lengkap
     <br>
-    <input type="text" class="form control" style="width: 300px" name="EPNamaLenkap">
+    <input type="text" class="form control" style="width: 300px" name="EPNamaLengkap" value="{{$CUser->fullname}}">
     <br><br>
     Nomor Telepon
     <br>
-    <input type="text" class="form control" style="width: 300px" name="EPNoTelp">
+    <input type="text" class="form control" style="width: 300px" name="EPNoTelp" value="{{$CUser->user_telp}}">
     <br><br>
     <button class="btn btn-success">Save</button>
 
