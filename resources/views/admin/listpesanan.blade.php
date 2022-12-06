978af8c145ef873575f889e362123864c8e01b9c
jonathankl <jonathan_k20@mhs.istts.ac.id>
2022-12-06 23:24:49 +0700

    zoom bukti pembayaran

--- resources/views/admin/listpesanan.blade.php
+++ resources/views/admin/listpesanan.blade.php
@@ -50,8 +50,36 @@
             </td>
             <td>
                 @if ($h[$i]->bukti != null)
+                <a class="portfolio-link" data-bs-toggle="modal" href="{{url('#portfolioModal'.$i)}}">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img src="{{asset('/storage/bukti/'.$h[$i]->bukti)}}" class="class="img-fluid"" style="width: 100px; height:100px ;">
 
-                <img src="{{asset('/storage/bukti/'.$h[$i]->bukti)}}" style="width: 100px; height:100px ;">
+                </a>
+
+                <div class="portfolio-modal modal fade" id="portfolioModal{{$i}}" tabindex="-1" role="dialog" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            {{-- <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div> --}}
+                            <div class="container">
+                                <div class="row justify-content-center">
+                                    <div class="col-lg-8">
+                                        <div class="">
+
+                                            <img class="img-fluid d-block mx-auto" src="{{asset('/storage/bukti/'.$h[$i]->bukti)}}" alt="..." />
+
+                                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                                <i class="fas fa-xmark me-1"></i>
+                                                Close
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 @else
                     Belum Ada
 
